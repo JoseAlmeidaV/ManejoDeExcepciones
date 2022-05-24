d42034c8d88ce3cac5f142c561d67c141b5f298b
JoseAlmeidaV <105743123+JoseAlmeidaV@users.noreply.github.com>
2022-05-24 11:15:21 -0700

    24/05/22
    trabajo en clase 24/05/22

--- ManejodeExcepciones/ManejodeExcepciones/Program.cs
+++ ManejodeExcepciones/ManejodeExcepciones/Program.cs
@@ -10,6 +10,25 @@ namespace ManejodeExcepciones
     {
         static void Main(string[] args)
         {
+            int x;
+
+
+            try
+            {
+                Console.Write("Coloque un segundo numero: ");
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine("valor al cuadrado: " + (Math.Pow(x, 2)));
+                Console.ReadKey();
+            }
+            catch (FormatException e )
+            {
+                Console.WriteLine("Mensaje de error :" + e.Message);
+                Console.ReadKey();
+            }
+            finally
+            {
+
+            }
         }
     }
 }
